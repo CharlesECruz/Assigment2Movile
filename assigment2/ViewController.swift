0ce84a72bd879cae1aaf17a2920ec64406be0b27
Charles <carloseduarcma@gmail.com>
2020-04-29 18:18:45 -0700

    finished

---
+++ assigment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assigment2
+//
+//  Created by Carlos Camacho on 2020-04-29.
+//  Copyright © 2020 Carlos Camacho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
